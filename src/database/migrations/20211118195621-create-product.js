c6eb812da90e7bbfce882cce8df41320c31bf9e4
CinRigoni <cinthia.rigoni@alumnos.frm.utn.edu.ar>
2021-11-18 16:58:40 -0300

    Actualizando

---
+++ src/database/migrations/20211118195621-create-product.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      stockMin: {
+        type: Sequelize.INTEGER
+      },
+      stockMax: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      weight: {
+        type: Sequelize.DECIMAL
+      },
+      dimensions: {
+        type: Sequelize.STRING
+      },
+      brandsId: {
+        type: Sequelize.INTEGER
+      },
+      categoriesId: {
+        type: Sequelize.INTEGER
+      },
+      visibilitiesId: {
+        type: Sequelize.INTEGER
+      },
+      discountsId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
